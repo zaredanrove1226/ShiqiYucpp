d3d2231ef72c4d7b5fe4e47a87abd3232e6ca3d8
zaredanrove <zaredanrove@qq.com>
2025-09-21 16:38:16 +0800

    test

--- week03/examples/while.cpp
+++ week03/examples/while.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 int main()
 {
-    int num = 10;
-    while(num > 0)
-    {
-        cout << "num = " << num << endl;
-        num--;
-    }
+    // int num = 10;
+    // while(num > 0)
+    // {
+    //     cout << "num = " << num << endl;
+    //     num--;
+    // }
 
-    // num = 10;
+    // int num = 10;
     // do
     // {
     //     cout << "num = " << num << endl;
     //     num--;
     // }while (num > 0);
 
-    // num = 10;
-    // while (num > 0)
-    // {
-    //     if (num == 5)
-    //         break;
-    //     cout << "num = " << num << endl;
-    //     num--;
-    // }
+    int num = 10;
+    while (num > 0)
+    {
+        if (num == 5)
+            break;
+        cout << "num = " << num << endl;
+        num--;
+    }
     return 0;
 } 
