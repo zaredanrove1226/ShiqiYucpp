01032de693a73d4205de3140a1a538ff2ae44639
zaredanrove <zaredanrove@qq.com>
2025-09-21 17:15:32 +0800

    test

--- week03/examples/while.cpp
+++ week03/examples/while.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-    // int num = 10;
-    // while(num > 0)
-    // {
-    //     cout << "num = " << num << endl;
-    //     num--;
-    // }
+    int num = 10;
+    while(num > 0)
+    {
+        cout << "num = " << num << endl;
+        num--;
+    }
 
     // int num = 10;
     // do
@@ -16,13 +16,14 @@ int main()
     //     num--;
     // }while (num > 0);
 
-    int num = 10;
-    while (num > 0)
-    {
-        if (num == 5)
-            break;
-        cout << "num = " << num << endl;
-        num--;
-    }
-    return 0;
-} 
+    // int num = 10;
+    // while (num > 0)
+    // {
+    //     if (num == 5)
+    //         break;
+    //     cout << "num = " << num << endl;
+    //     num--;
+    // }
+    // return 0;
+}
+// 一定要注意==不能写成=，==是相等，=是赋值，while的条件如果写成赋值语句，可能会造成死循环
