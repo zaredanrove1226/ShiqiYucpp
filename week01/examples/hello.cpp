78114546f5a304e73223d0055242cc466f581b7d
zaredanrove <zaredanrove@qq.com>
2025-09-21 16:16:47 +0800

    annotation & compilation

--- week01/examples/hello.cpp
+++ week01/examples/hello.cpp
@@ -4,6 +4,10 @@
 #include <string>
 
 using namespace std;
+// 将标准库（Standard Library）中的所有标识符（如 cout、vector、string）
+// 从 std命名空间暴露到当前作用域，​​无需再写 std::前缀
+
+// using namespace std;的作用域从声明处开始，直到当前代码块结束（如函数、文件）
 
 int main()
 {
--- week02/examples/precision.cpp
+++ week02/examples/precision.cpp
@@ -12,4 +12,7 @@ int main()
     cout << "f1 - f2 = " << f1 - f2 << endl;
     cout << "(f1 - f2 == 0) = " << (f1 - f2 == 0) << endl;
     return 0;
+    // if(fabs(f1 - f2) < FLT_EPSILON)
 }
+// 因为 float的精度有限，当进行 f1 + 10这个操作时，
+// 计算结果被硬件​​舍入​​到了最接近那个可以被 float表示的数，而这个数恰好就是 f1本身
